94562b776d849c3d2aa21f8249b204ff49444ad4
Luca Vinciguerra <lvinci@outlook.de>
2025-01-14 19:03:55 +0100

    fix example app

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import 'dart:async';
-
-import 'package:flutter/services.dart';
 import 'package:paypal_web_payments/paypal_web_payments.dart';
 
 void main() {
@@ -16,35 +13,16 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  String _platformVersion = 'Unknown';
-  final _paypalWebPaymentsPlugin = PaypalWebPayments();
+  // ignore: unused_field
+  final _paypalWebPaymentsPlugin = PaypalPaymentService(
+    onFailureCallback: (int code, String description) {},
+    onCanceledCallback: () {},
+    onSuccessCallback: (String orderId, String payerId) {},
+  );
 
   @override
   void initState() {
     super.initState();
-    initPlatformState();
-  }
-
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initPlatformState() async {
-    String platformVersion;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    // We also handle the message potentially returning null.
-    try {
-      platformVersion =
-          await _paypalWebPaymentsPlugin.getPlatformVersion() ?? 'Unknown platform version';
-    } on PlatformException {
-      platformVersion = 'Failed to get platform version.';
-    }
-
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) return;
-
-    setState(() {
-      _platformVersion = platformVersion;
-    });
   }
 
   @override
@@ -53,9 +31,6 @@ class _MyAppState extends State<MyApp> {
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Plugin example app'),
-        ),
-        body: Center(
-          child: Text('Running on: $_platformVersion\n'),
         ),
       ),
     );
